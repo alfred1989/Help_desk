f01c0bf814d91c0ea8faf26b813e8ff21d69cc6f
alfred-1989 <alfred-1989@o2.pl>
2020-10-26 07:51:32 +0100

    Initial commit

---
+++ src/main/java/hubb/demo/HeplDeskApplication.java
@@ -0,0 +1,13 @@
+package hubb.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeplDeskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HeplDeskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hubb/demo/HeplDeskApplicationTests.java
@@ -0,0 +1,13 @@
+package hubb.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeplDeskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
