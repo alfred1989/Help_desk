27c9ebd63ab2ccc7314e87d483eca7559d2c9226
alfred-1989 <alfred-1989@o2.pl>
2020-10-26 17:38:51 +0100

    test ..

---
+++ src/main/java/hubb/demo/Controller/HomeController.java
@@ -0,0 +1,16 @@
+package hubb.demo.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+@RequestMapping("/")
+    public String home(){
+    return "Hello Word";
+}
+
+
+
+
+}
